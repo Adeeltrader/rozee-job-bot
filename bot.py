23415bea5dd35e4bb1cfff9241d57a56615ba220
Auto Boss <adeelahmad786jrw1@outlook.com>
2025-03-27 02:52:50 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import webbrowser  
+
+# Rozee.pk open karne ka simple bot
+def open_rozee():
+    url = "https://www.rozee.pk"
+    webbrowser.open(url)
+
+# Bot ko run karna
+if __name__ == "__main__":
+    open_rozee()
